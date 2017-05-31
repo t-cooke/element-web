t-cooke/element-web

commit 0e742cec277ff3aa708ecaf4028627b3350a74ae

scripts/fetch-develop.deps.sh: set -e (#4120)
... so that when the git clone fails, it doesn't plough on regardless.

scripts/fetch-develop.deps.sh
@@ -6,6 +6,8 @@
 # the branch the current checkout is on, use that branch. Otherwise,
 # use develop.
 
+set -e
+
 # Look in the many different CI env vars for which branch we're
 # building
 if [[ "$TRAVIS" == true ]]; then

commit 4586e25974fd04345709059a8eb0f0e05d77dee8

Fix fetch-develop-deps.sh
This got broken by https://github.com/vector-im/riot-web/pull/4120 so that it
didn't work on a clean git clone.

scripts/fetch-develop.deps.sh
@@ -42,13 +42,13 @@ dodep matrix-org matrix-react-sdk
 mkdir -p node_modules
 cd node_modules
 
-rm -r matrix-js-sdk 2> /dev/null
+rm -r matrix-js-sdk 2> /dev/null || true
 ln -s ../matrix-js-sdk ./
 pushd matrix-js-sdk
 npm install
 popd
 
-rm -r matrix-react-sdk 2> /dev/null
+rm -r matrix-react-sdk 2> /dev/null || true
 ln -s ../matrix-react-sdk ./
 pushd matrix-react-sdk
 mkdir -p node_modules